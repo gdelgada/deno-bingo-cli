104dc3d08e92ac47b80fd5f41cb9173ad20bf213
Guille <132089900+gdelgada@users.noreply.github.com>
2025-01-02 12:35:31 +0100

    Add .gitignore and enhance Deno configuration with additional tasks and formatting updates

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "@std/assert";
-import { add } from "../src/main.ts";
+import { assertEquals } from '@std/assert';
+import { add } from '../src/main.ts';
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+	assertEquals(add(2, 3), 5);
 });
