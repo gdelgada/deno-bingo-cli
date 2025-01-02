832cbc17ccfc4bc775e111ccdf1f440541b99185
Guille <132089900+gdelgada@users.noreply.github.com>
2025-01-02 13:48:14 +0100

    Add drum service implementation and update main logic; remove old add function test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-export function add(a: number, b: number): number {
-	return a + b;
-}
+import {
+	createDrum,
+	getBallRandomly,
+	getFalsePositions,
+} from './services/drum.ts';
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-	console.log('Add 2 + 3 =', add(2, 3));
+	const numberLength = 5;
+	let drum = createDrum(numberLength);
+	while (getFalsePositions(drum).length > 0) {
+		console.log();
+		drum = getBallRandomly(drum);
+		console.log(drum);
+	}
 }
