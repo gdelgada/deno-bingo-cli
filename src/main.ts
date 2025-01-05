b7fcadce8775cc434ab042af8ce99debbe3655f1
Guille <132089900+gdelgada@users.noreply.github.com>
2025-01-05 11:31:31 +0100

    Implement interactive drum game with user prompts and display; refactor main loop

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,48 @@ import {
 	getFalsePositions,
 } from './services/drum.ts';
 
+function printDrum(drum: boolean[]) {
+	const columns = 10;
+	const rows = Math.ceil(drum.length / columns);
+	const table = [];
+
+	for (let i = 0; i < rows; i++) {
+		const rowValues = drum.slice(i * columns, (i + 1) * columns);
+		const row: (number | null | string | undefined)[] = [];
+		for (let index = 0; index < rowValues.length; index++) {
+			const ball = i * columns + index + 1;
+			row.push(rowValues[index] ? ball : '');
+		}
+		table.push(row);
+	}
+	console.table(table);
+}
+
+function play() {
+	try {
+		const drumLengthInput = prompt('Drum length:', '90');
+		const drumLength = parseInt(drumLengthInput!);
+		if (isNaN(drumLength)) {
+			throw new Error('The input is not a valid integer.');
+		}
+		let drum = createDrum(drumLength);
+		console.clear();
+		printDrum(drum);
+		while (getFalsePositions(drum).length > 0) {
+			alert('New ball?');
+			console.clear();
+			drum = getBallRandomly(drum);
+			printDrum(drum);
+		}
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
 if (import.meta.main) {
-	const numberLength = 5;
-	let drum = createDrum(numberLength);
-	while (getFalsePositions(drum).length > 0) {
-		console.log();
-		drum = getBallRandomly(drum);
-		console.log(drum);
+	while (true) {
+		if (confirm('Do you want to play?')) {
+			play();
+		}
 	}
 }
